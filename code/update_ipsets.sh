1aac5cfb4919af834403014c59d9fceef74b91a2
Adrián Jiménez Gamo <ajotage10@gmail.com>
2025-07-07 17:45:27 +0200

    clarify

--- code/update_ipsets.sh
+++ code/update_ipsets.sh
@@ -10,8 +10,7 @@ SCRIPT5="sudo python3 update_ipmasks.py bruteforceblocker"
 sudo update-ipsets run bds_atif bitcoin_nodes blocklist_de blocklist_de_apache blocklist_de_bots blocklist_de_bruteforce blocklist_de_ftp blocklist_de_imap blocklist_de_mail blocklist_de_sip blocklist_de_ssh blocklist_de_strongips blocklist_net_ua botscout botvrij_dst botvrij_src bruteforceblocker ciarmy cleantalk_new cleantalk_new_1d cleantalk_updated cleantalk_top20 cleantalk cleantalk_1d cleantalk_7d cleantalk_30d cybercrime dataplane_dnsrd dataplane_sipinvitation dataplane_sipquery dataplane_sipregistration dataplane_sshclient dataplane_sshpwauth dataplane_vncrfb dm_tor et_compromised et_dshield et_tor fullbogons gpf_comics greensnow iblocklist_ads iblocklist_ciarmy_malicious iblocklist_edu iblocklist_level2 iblocklist_level3 iblocklist_spyware myip php_commenters php_dictionary php_harvesters php_spammers sblam socks_proxy spamhaus_drop sslproxies stopforumspam tor_exits vxvault yoyo_adservers
 
 # replace with the path to code
-cd /abastor/adrian/TFM/code
-# cd /home/ajotagesr/Documents/GitHub/TFM/code/src
+cd $PATH_TO_CODE
 
 
 # delete repository if it already exists
@@ -24,7 +23,7 @@ git clone https://github.com/firehol/blocklist-ipsets.git
 
 # replace with path to the folder where your virtual env is
 # or delete if it doesn't apply to your case
-cd /abastor/adrian/TFM/code
+cd $PATH_TO_CODE
 source venv/bin/activate
 cd src
 
